a818b4ca4878c225a7ff7cacef7e6af2be7b01f4
Evan Tomczyk <tomczykej@gmail.com>
2019-07-03 22:55:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return array;
+}
+
+function johnLennonFacts(facts) {
+  var i = 0
+  while (i < facts.length) {
+    facts[i] = facts[i] + '!!!';
+    i++;
+  }
+  return facts;
+}
